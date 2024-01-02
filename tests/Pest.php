4fbbb12cce4c615fd5d23a55defd4c87ced5019e
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2024-01-02 23:50:22 +0000

    WIP

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,7 @@
 <?php
 
 use CodebarAg\Zammad\Tests\TestCase;
+use CodebarAg\Zammad\Zammad;
 use Illuminate\Support\Facades\Event;
 
 uses(TestCase::class)->in(__DIR__);
@@ -8,3 +9,22 @@ uses(TestCase::class)->in(__DIR__);
 uses()->beforeEach(function () {
     Event::fake();
 })->in(__DIR__);
+
+uses()
+    ->beforeEach(function () {
+        // Delete all tickets before each test
+        $tickets = (new Zammad())->ticket()->list();
+
+        foreach ($tickets as $ticket) {
+            (new Zammad())->ticket()->delete($ticket->id);
+        }
+
+        // Delete all users before each test
+        $users = (new Zammad())->user()->list();
+
+        foreach ($users as $user) {
+            (new Zammad())->user()->delete($user->id);
+        }
+
+    })
+    ->in(__DIR__);
