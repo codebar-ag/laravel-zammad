f272bb42876da657ee5477d5108aac81d3ca00fb
Sebastian Fix <sebastian.fix@codebar.ch>
2024-01-05 12:31:55 +0100

    WIP

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,7 +1,6 @@
 <?php
 
 use CodebarAg\Zammad\Tests\TestCase;
-use CodebarAg\Zammad\Zammad;
 use Illuminate\Support\Facades\Event;
 
 uses(TestCase::class)->in(__DIR__);
@@ -9,22 +8,3 @@ uses(TestCase::class)->in(__DIR__);
 uses()->beforeEach(function () {
     Event::fake();
 })->in(__DIR__);
-
-uses()
-    ->beforeEach(function () {
-        // Delete all tickets before each test
-        $tickets = (new Zammad())->ticket()->list();
-
-        foreach ($tickets as $ticket) {
-            (new Zammad())->ticket()->delete($ticket->id);
-        }
-
-        // Delete all users before each test
-        $users = (new Zammad())->user()->list();
-
-        foreach ($users as $user) {
-            (new Zammad())->user()->delete($user->id);
-        }
-
-    })
-    ->in(__DIR__);
